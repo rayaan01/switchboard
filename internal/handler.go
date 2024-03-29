e6123b1d59f8fabd631b9c29da95eab312938938
Rayaan Hussain <md.rayaan.h@gmail.com>
2024-03-29 14:38:39 +0000

    fix: move routing and operations

--- internal/handler.go
+++ internal/handler.go
@@ -40,51 +40,6 @@ func Handler(conn net.Conn, s *Server) {
 	}
 }
 
-func router(args []string) ([]byte, error) {
-	errorMessage := []byte("Available commands:\n1. set [key] [value]\n2. get [key]\n3. exit \n")
-	if len(args) == 0 {
-		return errorMessage, nil
-	}
-	cmd := strings.ToLower(args[0])
-	switch cmd {
-	case "set":
-		if len(args) != 3 {
-			return errorMessage, nil
-		}
-		key := args[1]
-		val := args[2]
-		response, err := handleSet(key, val)
-		if err != nil {
-			return nil, err
-		}
-		return append(response, []byte("\n")...), nil
-	case "get":
-		if len(args) != 2 {
-			return errorMessage, nil
-		}
-		key := args[1]
-		response, err := handleGet(key)
-		if err != nil {
-			return nil, err
-		}
-		return append(response, []byte("\n")...), nil
-	case "del":
-		if len(args) != 2 {
-			return errorMessage, nil
-		}
-		key := args[1]
-		response, err := handleDel(key)
-		if err != nil {
-			return nil, err
-		}
-		return append(response, []byte("\n")...), nil
-	case "exit":
-		return nil, io.EOF
-	default:
-		return errorMessage, nil
-	}
-}
-
 func read(buffer *[]byte, bytesRead *int, conn net.Conn) error {
 	for {
 		chunk := make([]byte, 128)
@@ -98,22 +53,4 @@ func read(buffer *[]byte, bytesRead *int, conn net.Conn) error {
 			return nil
 		}
 	}
-}
-
-func handleGet(key string) ([]byte, error) {
-	value, ok := Store[key]
-	if !ok {
-		return []byte("(nil)"), nil
-	}
-	return []byte(value), nil
-}
-
-func handleSet(key string, value string) ([]byte, error) {
-	Store[key] = value
-	return []byte("OK"), nil
-}
-
-func handleDel(key string) ([]byte, error) {
-	delete(Store, key)
-	return []byte("OK"), nil
 }
---
+++ internal/router.go
@@ -0,0 +1,51 @@
+package internal
+
+import (
+	"io"
+	"strings"
+)
+
+func router(args []string) ([]byte, error) {
+	errorMessage := []byte("Available commands:\n1. set [key] [value]\n2. get [key]\n3. exit \n")
+	if len(args) == 0 {
+		return errorMessage, nil
+	}
+	cmd := strings.ToLower(args[0])
+	switch cmd {
+	case "set":
+		if len(args) != 3 {
+			return errorMessage, nil
+		}
+		key := args[1]
+		val := args[2]
+		response, err := handleSet(key, val)
+		if err != nil {
+			return nil, err
+		}
+		return append(response, []byte("\n")...), nil
+	case "get":
+		if len(args) != 2 {
+			return errorMessage, nil
+		}
+		key := args[1]
+		response, err := handleGet(key)
+		if err != nil {
+			return nil, err
+		}
+		return append(response, []byte("\n")...), nil
+	case "del":
+		if len(args) != 2 {
+			return errorMessage, nil
+		}
+		key := args[1]
+		response, err := handleDel(key)
+		if err != nil {
+			return nil, err
+		}
+		return append(response, []byte("\n")...), nil
+	case "exit":
+		return nil, io.EOF
+	default:
+		return errorMessage, nil
+	}
+}
