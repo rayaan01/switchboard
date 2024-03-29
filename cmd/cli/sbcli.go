9176f3bb4fcfb9f53761d990ac325df53557c0fe
Rayaan Hussain <md.rayaan.h@gmail.com>
2024-03-29 16:03:47 +0000

    feat: basic repl cli

--- cmd/cli/sbcli.go
+++ cmd/cli/sbcli.go
@@ -1,1 +1,29 @@
-package cli
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func welcomePrompt() {
+	fmt.Println("Welcome to the switchboard CLI!")
+	fmt.Println("You can use this tool to interact with the switchboard db. Usage -")
+	fmt.Println("1. set [key] [value]")
+	fmt.Println("2. get [key]")
+	fmt.Println("3. del [key]")
+}
+
+func displayWrapper(text string) {
+	fmt.Printf("%s %s", "switchboard >>", text)
+}
+
+func main() {
+	welcomePrompt()
+	displayWrapper("")
+	reader := bufio.NewScanner(os.Stdin)
+	for reader.Scan() {
+		text := reader.Text()
+		displayWrapper(text)
+	}
+}
