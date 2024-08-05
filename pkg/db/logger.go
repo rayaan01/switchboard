eaea849a70438bc43816b1cea6b4e2a7821212ec
Rayaan Hussain <md.rayaan.h@gmail.com>
2024-08-05 15:14:53 +0100

    fix: logger and analysis

--- pkg/db/logger.go
+++ pkg/db/logger.go
@@ -3,6 +3,7 @@ package db
 import (
 	"encoding/csv"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -11,4 +12,13 @@ func logger(index int, key string, value string, duration float64, writer *csv.W
 	formattedIndex := fmt.Sprintf("%d", index)
 	executionTime := fmt.Sprintf("%.6f", duration)
 	writer.Write([]string{formattedIndex, timestamp, key, value, executionTime})
+}
+
+func generateRandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
